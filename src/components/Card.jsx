4ad516f1ba3c0549fa4fb9ccdb5662ba7cdc3847
DaniDItzMe <td.danix@gmail.com>
2026-03-27 21:30:45 +0100

    feat: bonus exercise completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react"
-
 export default function Card(props){
 
-    const [isOpen, setIsOpen] = useState(false);
+
 
     return (
         <div className="card">
-            <button className={isOpen && "open"} onClick={() => setIsOpen((current)=> !current)}>{props.language}</button>
-            {isOpen && <p>{props.description}</p>}
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
         </div>
     )
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,29 @@
 import languages from "../languages"
 import Card from "./Card"
+import { useState } from "react"
 export default function Main(){
 
+    const [activeButton, setActiveButton] = useState(languages[0].title)
+
+    console.log(languages.find(item => {return item.title === "HTML" ? item.description : languages[0].description}));
+    
     return(
         
         <main>
+            <div className="">
+                <ul className="buttons-container">
+                    {
+                        languages.map(language => (
+                            <li key={language.id}><button className={activeButton == language.title ? "open" : ""} onClick={()=> setActiveButton(language.title)}>{language.title}</button></li>
+                        ))
+                    }
+                </ul>
+                
+            </div>
             <div className="card-container">
                 
-                {
-                    languages.map(language =>(
-                        <Card key={language.id} language={language.title} description={language.description}></Card>
-                    ))
-                }
+                <Card title={activeButton} description={languages.find(item => item.title === activeButton ).description}></Card>
+
             </div>
         </main>
         
